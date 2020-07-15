1f258b82f0694ce31c4372abc7d8c9348348ab02
farmtab-dev <farmtab.app@gmail.com>
2020-07-15 22:54:03 +0800

    Check arduino code update

--- farmtab-arduino.ino
+++ farmtab-arduino.ino
@@ -89,7 +89,7 @@ void loop() {
   sdService.update();
 
   // ************************* Serial debugging ******************
-  if (millis() - updateTime > 2000)
+  if (millis() - updateTime > 5000)
   {
     updateTime = millis();
     Serial.print(F("PH@"));
